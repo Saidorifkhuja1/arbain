52c272530fd6de778c7a7f2854712b777c1d1207
Saidorifkhuja Yuldoshkhujaev <yoldoshxojayevsaidorifxoja@gmail.com>
2025-03-05 09:33:05 +0500

    arbain

--- user/models.py
+++ user/models.py
@@ -52,7 +52,7 @@ class User(AbstractBaseUser, PermissionsMixin):
     objects = UserManager()
 
     USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = ['name', 'last_name']
+    REQUIRED_FIELDS = ['name', 'last_name', 'phone_number']
 
     def __str__(self):
         return f'{self.name} {self.last_name}'
