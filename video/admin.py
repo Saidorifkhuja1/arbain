0b68069c9e882964d11f7dae9eb435136c2de3b1
Saidorifkhuja Yuldoshkhujaev <yoldoshxojayevsaidorifxoja@gmail.com>
2025-03-14 11:54:57 +0500

    arbain

--- video/admin.py
+++ video/admin.py
@@ -1,8 +1,18 @@
-from django.contrib import admin
+from django import forms
 from .models import Videos
+from django.contrib import admin
+
+
+
+class VideosAdminForm(forms.ModelForm):
+    class Meta:
+        model = Videos
+        fields = '__all__'
+        widgets = {
+            'video': forms.ClearableFileInput(attrs={'accept': 'video/*'}),
+        }
 
 @admin.register(Videos)
 class VideosAdmin(admin.ModelAdmin):
+    form = VideosAdminForm
     list_display = ['title']
-
-    
--- video/models.py
+++ video/models.py
@@ -1,13 +1,19 @@
 import uuid
 from django.db import models
 from django.utils import timezone
+from django.core.exceptions import ValidationError
+
+def validate_video_file_extension(value):
+    valid_extensions = ['.mp4', '.avi', '.mov', '.mkv']
+    if not any([value.name.lower().endswith(ext) for ext in valid_extensions]):
+        raise ValidationError("Unsupported file extension. Please upload a video file.")
 
 class Videos(models.Model):
     uid = models.UUIDField(default=uuid.uuid4, editable=False, primary_key=True)
     title = models.CharField(max_length=250)
     body = models.TextField()
-    image = models.ImageField(upload_to='videos/')
-    video = models.ImageField(upload_to='videos/')
+    image = models.ImageField(upload_to='videos/', max_length=10000000)
+    video = models.FileField(upload_to='videos/', validators=[validate_video_file_extension], max_length=10000000)
     uploaded_at = models.DateTimeField(default=timezone.now)
 
     def __str__(self):
